89593a4cddba72a52a58d23020b2c484c1a7e3e6
Bryny <28590441+BrynyPatchett@users.noreply.github.com>
2024-03-08 14:54:42 +1300

    comment update

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,7 +2,8 @@ exports.index = function (req, res,){
 
     //load all messages from db, and pass them to the view
     //pass the user object to the view, will be undefined if not logged in 
-    // view decides: Not logged in/ logged in not a member, show messages w/o names
+    // view decides: Not logged in/ logged in not a member, show messages w/o names,
+    // logged in has option to create
     // member, sees names
     // admin sees delete option
     res.send("NOT IMPLEMENTED: Message Controller Index Page")
